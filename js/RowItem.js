24e6fdee0998a57a569ad057ce59ab4d9fcc35cd
GanahaHibiki <toshilang@hotmail.com>
2020-03-15 22:46:57 +0800

    fix table

--- js/RowItem.js
+++ js/RowItem.js
@@ -1,30 +1,23 @@
 class RowItem extends React.Component {
-  // return (
-  //   <div className="shopping-list">
-  //     <h1>Shopping List for {this.props.name}</h1>
-  //     <ul>
-  //       <li>Instagram</li>
-  //       <li>WhatsApp</li>
-  //       <li>Oculus</li>
-  //     </ul>
-  //   </div>
-  // );
   constructor(props) {
     super(props);
   }
 
   render() {
+    let imgs = [];
+    this.props.cardUris.forEach((uri, index) => {
+      imgs.push(
+        (<img key={+new Date() + "_" + this.props.rowIndex + "_" + index} src={uri}></img>)
+      )
+    });
+
+
     return (
       <tr id="scoreRow">
-          <td style="align=center">this.props.rowIndex</td>
-          <td>
-            {
-              this.props.cardUris.map((uri, index) => {
-                return
-                  (<img key={+new Date() + "_" + this.props.rowIndex + "_" + index} src={uri}></img>)
-              })
-            }
-          </td>
+        <td style={{ align: 'center' }}>{this.props.rowIndex}</td>
+        <td>
+          {imgs}
+        </td>
       </tr>
     );
   }
