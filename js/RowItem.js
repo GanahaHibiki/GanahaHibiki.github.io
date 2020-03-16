d66f2d9b0542549d9a606dc2e28a4db4b950fe95
GanahaHibiki <14134696+GanahaHibiki@users.noreply.github.com>
2020-03-16 23:11:26 +0800

    add desc for icons

--- js/RowItem.js
+++ js/RowItem.js
@@ -6,16 +6,22 @@ class RowItem extends React.Component {
   render() {
     let imgs = [];
     this.props.cardUris.forEach((uri, index) => {
+      let fileName = uri.split("/")[2].split(".")[0];
       imgs.push(
-        (<img key={+new Date() + "_" + this.props.rowIndex + "_" + index} style={{height: '8rem'}} src={uri}></img>)
+        (
+          <span className="IconBox">
+            <img key={+new Date() + "_" + this.props.rowIndex + "_" + index} style={{height: '8rem'}} src={uri}></img>
+            <em style={{"font-style": "normal"}}>{fileName}</em>
+          </span>
+        )
       )
     });
 
 
     return (
-      <tr id="scoreRow">
-        <td style={{ align: 'center' }}>{+this.props.rowIndex + 1}</td>
-        <td>
+      <tr>
+        <td style={{ "align": 'center' }}>{+this.props.rowIndex + 1}</td>
+        <td className="RowBox">
           {imgs}
         </td>
       </tr>
